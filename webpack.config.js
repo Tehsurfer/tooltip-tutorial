77407b29b359cff78d79a3fbd9f6ef055c41a5ea
Jesse Khorasanee <jessekhorasanee@gmail.com>
2019-09-09 17:05:42 +1200

    Disable watching of files for netlify

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,6 @@ const path = require('path');
 module.exports = {
   mode: 'production',
   entry: path.join(__dirname, 'src', 'index'),
-  watch: true,  
-
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: "[name].js",
